d09a4fa511e1653efc90d38e4f64b1419ef04376
Woo Jung <wooseokjung0826@gmail.com>
2018-11-17 15:36:21 -0800

    updated config app name to Loggregate and edited welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,12 +85,9 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="~ubuntu/proposal.html">
+                    Proposal</a>
+                    <a href="https://github.com/wsjung/loggregate">GitHub</a>
                 </div>
             </div>
         </div>
