d23c8589a23f4b0ca7ac6e94035da1a667c4e7c9
Woo Jung <wooseokjung0826@gmail.com>
2018-11-17 18:48:56 -0800

    scrape_course.py scrapes coursicle for all the subjects and courses that UPS offers. created DDL file for test database containing Courses table

---
+++ database/scripts/scrape_course.py
@@ -0,0 +1,56 @@
+from bs4 import BeautifulSoup
+from requests import get
+import time
+import sqlite3
+
+# constants
+# SQLITE_FILE = './../database.sqlite'
+SQLITE_FILE = './foo.sqlite'
+URL='https://www.coursicle.com/pugetsound/courses/'
+
+# sqlite db connection
+connection = sqlite3.connect(SQLITE_FILE)
+cursor = connection.cursor()
+print('database connection opened')
+
+# GET request the URL
+response = get(URL).text
+
+# beautiful soup
+soup = BeautifulSoup(response, features='html.parser')
+
+# extract list of departments
+depts = soup.find_all('span', {'class' : 'tileElementText'})
+depts = [d.text for d in depts]
+
+print(depts)
+
+for dept in depts:
+	print(dept, flush=True)
+
+	# response
+	dept_resp = get(URL + dept).text
+	# print(dept_resp)
+
+	# soup creation
+	dept_soup = BeautifulSoup(dept_resp, features='html.parser')
+
+	# course numbers
+	courses = dept_soup.find_all('span', {'class' : 'tileElementText'})
+	courses = [c.text for c in courses]
+
+	subjects = [c.split()[0] for c in courses]
+	nums = [c.split()[1] for c in courses]
+
+	# course titles
+	titles = dept_soup.find_all('div', {'class' : 'tileElementHiddenText'})
+	titles = [t.text.replace("\"","'") for t in titles]
+
+	for s,n,t in zip(subjects,nums,titles):
+		print("insert into Courses values ('%s',%s,'%s')" % (s,n,t), flush=True)
+		cursor.execute("insert into Courses values ('%s',%s,\"%s\")" % (s,n,t))
+
+connection.commit()
+print('database changes committed')
+connection.close()
+print('database connection closed')
