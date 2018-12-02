560001602e74213491fec12b73fcc8112ee7ce4f
Conor Muldoon <43060779+SpudBoi@users.noreply.github.com>
2018-12-02 12:10:07 -0800

    Add files via upload

--- resources/views/grouphome.blade.php
+++ resources/views/grouphome.blade.php
@@ -2,10 +2,36 @@
 
 @section('content')
 <main role="main" class="container">
-    <div class="jumbotron jumbotron-fluid">
+    <div class="jumbotron text-center">
         <div class="container">
-            <h1 class="display-4" align="center">[Group Name]</h1>
+            <h3 class="display-4"><?php echo $studygroup[0]->name; ?></h3>
+            <h5>Meet Day: <?php echo $studygroup[0]->meetDay ?><br/>Meet Time: <?php echo $studygroup[0]->meetTime ?><br/>Meet Location: <?php echo $studygroup[0]->meetLocation ?></h5>
+            <p align="center">
+                <button class="btn btn-primary my-2">Join Group</button>
+            </p>
         </div>
     </div>
+
+    <div class="container">
+    <div class="row">
+
+        <div class="col-sm-8">
+            <div class="panel panel-white post panel-shadow">
+                <div class="post-heading">
+                    <div class="pull-left meta">
+                        <div class="title h5">
+                            <a href="#"><b>Conor Muldoon</b></a>
+                            made a post.
+                        </div>
+                        <h6 class="text-muted time">1 minute ago</h6>
+                    </div>
+                </div>
+                <div class="post-description">
+                    <p>Eat my ass Derek Albosta!!! Hahahaha see you in class buddy!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </main>
 @endsection
