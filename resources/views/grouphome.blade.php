11fb977af8a44d63139ba0ee246da10fdb01b7d6
Conor Muldoon <43060779+SpudBoi@users.noreply.github.com>
2018-12-01 18:15:13 -0800

    Uploading new blade files

---
+++ resources/views/grouphome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<main role="main" class="container">
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4" align="center">[Group Name]</h1>
+        </div>
+    </div>
+</main>
+@endsection
