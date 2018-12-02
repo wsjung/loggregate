339229a0771380873098f0aae0428d3477b9fa85
Conor Muldoon <43060779+SpudBoi@users.noreply.github.com>
2018-12-01 16:03:07 -0800

    Added database migrations
    Completed subscribed, comments, membership, and studygroup table

---
+++ database/migrations/2018_12_01_233546_create_membership_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembershipTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('membership', function (Blueprint $table) {
+            $table->foreign('id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade'); #Foreign Key from users
+            $table->foreign('groupID')->references('groupID')->on('studygroup')->onUpdate('cascade')->onDelete('cascade'); #Foreign Key from studygroup
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('membership');
+    }
+}
