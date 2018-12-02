07df46b24a63283186807eac6224553d7fb18e76
Conor Muldoon <43060779+SpudBoi@users.noreply.github.com>
2018-12-01 16:59:31 -0800

    Add files via upload

---
+++ resources/views/coursehome.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<main role="main" class="container">
+    <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">[Course Name]</h1>
+  </div>
+</div>
+
+<div class="container" id="selectedCourses">
+</div>
+
+
+<div class=container>
+
+</div>
+</main>
+@endsection
