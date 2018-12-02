2e5f33b8e2688ccd96847aa5a96854f5eae61b70
Woo Jung <wooseokjung0826@gmail.com>
2018-12-01 16:26:52 -0800

    fixed up multiple selection in course search

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -49,7 +49,8 @@
     // functions for adding selected element to top
     function addCourse(subject, courseID) {
         // adds selected course to top
-        // if(document.getElementById('item'+subject+courseID).style.backgroundColor == '#ffffff') {
+        var l = document.getElementById('item' + subject + courseID);
+        if(l.style.backgroundColor === "rgb(255, 255, 255)" || l.style.backgroundColor === "") {
             var p = document.getElementById('selectedCourses');
             var newCourse = document.createElement('button');
             newCourse.setAttribute('class', 'btn btn-outline-primary');
@@ -59,9 +60,8 @@
             p.appendChild(newCourse);
 
             // recolors selected course in list
-            var p = document.getElementById('item' + subject + courseID);
-            p.style.backgroundColor = '#3399ff';
-        // }
+            l.style.backgroundColor = '#3399ff';
+        }
     }
 
     // function for removing select element from top
@@ -70,7 +70,7 @@
         var id = element.id.slice(8);
         element.parentNode.removeChild(element);
         var p = document.getElementById('item' + id);
-        p.style.backgroundColor = '#ffffff';
+        p.style.backgroundColor = '#fff';
     }
 </script>
 @endsection
