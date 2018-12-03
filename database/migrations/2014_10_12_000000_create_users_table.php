4694eb8782fc37dcb0823027c8c73db7ad97a927
Woo Jung <wooseokjung0826@gmail.com>
2018-12-03 12:34:49 -0800

    added default profile picture

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->string('avatar')->default('user.jpg');
+            $table->string('avatar')->default('user.png');
         });
     }
 
