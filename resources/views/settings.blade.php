2d6396cddb1ee7eea3e57eb3cf6320b47e5ddc1e
Woo Jung <wooseokjung0826@gmail.com>
2018-12-03 12:44:33 -0800

    updated search page for clarity

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -29,18 +29,21 @@
 
             <div class="profile-header-container">
                 <div class="profile-header-img">
-                    <img width="200px" height="200px" class="rounded-circle" src="/storage/avatars/{{ $user->avatar }}" />
+                    <img width="200px" height="200px" class="rounded-circle" src="/storage/avatars/{{ $user->avatar }}">
                     <div align="center" class="rank-label-container">
-                        <span class="label label-default rank-label">{{$user->name}}</span>
+                        <br>
+                        <span class="label label-default rank-label"><h4>{{$user->name}}</h4></span>
                     </div>
                 </div>
             </div>
 
         </div>
+        <br>
         <div class="row justify-content-center">
             <form action="/settings" method="post" enctype="multipart/form-data">
                 @csrf
                 <div class="form-group">
+                    <label for="avatar"><h4>Change profile picture</h4></label>
                     <input type="file" class="form-control-file" name="avatar" id="avatarFile" aria-describedby="fileHelp">
                     <small id="fileHelp" class="form-text text-muted">Please upload a valid image file. Size of image should not be more than 2MB.</small>
                 </div>
